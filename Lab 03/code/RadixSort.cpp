0a591a11c9a1deeed13e015ac8ef5bcfc8b54fe1
Randy Lam <randylam@Randys-MBP-2.attlocal.net>
2017-09-28 00:32:34 -0700

    more bugs woo

--- Lab 03/code/RadixSort.cpp
+++ Lab 03/code/RadixSort.cpp
@@ -1,74 +1,82 @@
-
+#include <math.h>
 #include <iostream>
 #include <stdlib.h>
-#include <string.h>
-#include <vector>
+#include <string>
 
 using namespace std;
 
-
- void MyFunc(T *array, int place, int size) {  
-    int pHolder[10];
-    T* end;
-    end = new T[size];
-    
-
-    for (int i = 0; i < 10; i++) {
-      end[i] = 0;
-    }
-
-    for (int j = 0; i < size; j++) {
-      end[array[i].key[place]];
-    }
-
-    for(int x = 0; x < 10; x++) {
-      end[x] = end[x] + end[x-1];
-    }
-
-    for (int y = size -1; y > 0; y--) {
-      result[end[array[i].key[place]]-1] = array[i];
-      end[array[i].key[place]]--;
-    }
-}
-
-// Radix sorts out index by index 
-// We only need to compare 10 spots so from 0 - 9
-void radixS(T* array, int arraySize) {
-  for (int i = 0; i < 10; i++) {
-    // Send an array, size, and place
-    MyFunc(array,i,arraySize);
-  }
-}
-
-struct T {
-  int K[10];
+struct V{
+	int key[10];
 };
 
+void myFunc(T* array, int pos, int size)
+{
+	T* result;
+	result = new V[size];
+	int count[10];
+	for(int i = 0; i<10; i++)
+		count[i]= 0;
+
+	for(int j = 0; j<size;j++) {
+		count[array[j].key[size]]++;
+	}
+
+	for(int x = 1; x<10; x++) {
+		count[x] = count[x]+count[x-1];
+	}
+
+	for(int y = size-1; y >=0; y--)
+	{
+		result[count[array[y].key[size]]-1] = array[y];
+		count[array[y].key[size]]--;
+	}
+
+	for(int i = 0; i<size; i++)
+		array[i] = result[i];
+	delete result;
+}
+
+void radixS (T* array, int size) {  
+	for(int i = 9; i >=0; i--)
+	{
+		myFunc(array, i, size);
+	}
+}
+
+
 int main(int argc,char **argv) {
-
   int arraySize;
-  int value;
 
+  // Get the size of the sequence
+  cin >> arraySize;
+
+  // Allocate enough memory to store "arraySize" integers
   T* array;
-  array = new T [arraySize];
+  array = new V [arraySize];
+    
+  // Read in the sequence
 
-  //Making a two dimensional ghetto array to store vector values
-  for (int x = 0; x < arraSize; x++) {
-    // Size of 10 because there's only 0-9 placesible values
-    for (int y = 0; y < 10; y++) {
-      cin >> array[x].K[j];
-    }
+  for(int i = 0; i<arraySize; i++)
+  {
+	for(int j = 0; j<10; j++)
+	{
+		cin>>array[i].key[j];
+//		cout<<"\nINPUT: "<<buffer;
+//		cout<<"\nVAL: "<<val;
+	}
   }
+  // Run your algorithms to manipulate the elements in Sequence
+  radixS(array, arraySize); 
+    
+  // Output the result
+  for(int x=0; i<arraySize; x++)
+      {
+	for(int y = 0; j<10;y++)
+		cout<<array[x].key[y]<<";";
+	cout<<endl;
+      }
+    
+  // Free allocated space
+  delete[] array;
+}
 
-  radixS(array,arraySize);
-
-  //Output
-  for (int x = 0; x <arraySize; x++) {
-    //Range is from 0 to 10 again cause values range from 0-9
-    for (int y = 0; y < 10; y++) {
-      cout << array[x].key[y] << "\n";
-    }
-  }
-   delete[] Sequence;
-  }
- 
