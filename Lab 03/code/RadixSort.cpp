a072b69d23de63a024cdd87334299b3408e8d82d
Randy Lam <randylam@Randys-MBP-2.attlocal.net>
2017-09-27 23:47:16 -0700

    lab3

--- Lab 03/code/RadixSort.cpp
+++ Lab 03/code/RadixSort.cpp
@@ -1,83 +1,74 @@
 
 #include <iostream>
 #include <stdlib.h>
-#include <string>
+#include <string.h>
+#include <vector>
 
 using namespace std;
 
-int maxValue(int *array, int size) {
 
-  int max = 0;
-  // If it's an array size of 1, it's the max
-  max = array[0];
-  for (int i = 0; i < size; i++) {
-    if (array[i] > max) {
-      max = array[i];
-    }
-  }
-  return max;
-}
-
-void MyFunc ( int *array, int size ) {  
+ void MyFunc(T *array, int place, int size) {  
+    int pHolder[10];
+    T* end;
+    end = new T[size];
     
 
-    int max = maxValue(array, size);
-    int tempArray[size];
-    int placeHolder[10] = {0}; // Possible index values 0-9 {0,0,0,0,0,0,0,0,0,0}
-    int index = 1;
-    int i = 0;
+    for (int i = 0; i < 10; i++) {
+      end[i] = 0;
+    }
 
-    // Checks to see if max number goes out index bounds
-   if ((max/index) > 0) {
-      for (i = 0; i < size; i ++) {
-        //Increments a placeHolder's 0-9 index so a number's last index is accounted
-        placeHolder[(array[i]/index) % 10]++;
-      }
-      //
-      for (i = 1; i < 10; i++) {
-        placeHolder[i] += placeHolder[i-1];
-      }
-      // Store the values within the placeHolder into a tempArray
-      // In placeHolders, the lowest value is on the bottom of the placeHolder so we have to go max size to retrieve it
-      // Store into array for now
-     for (i = size - 1; i >= 0; i--) {
-        tempArray[--placeHolder[(array[i]/index) % 10]] = array[i];
-      }
+    for (int j = 0; i < size; j++) {
+      end[array[i].key[place]];
+    }
 
-      for (i = 0; i < size; i ++) {
-        array[i] = tempArray[i];
-      }
-      index = index * 10; 
-   }  
-   
+    for(int x = 0; x < 10; x++) {
+      end[x] = end[x] + end[x-1];
+    }
+
+    for (int y = size -1; y > 0; y--) {
+      result[end[array[i].key[place]]-1] = array[i];
+      end[array[i].key[place]]--;
+    }
 }
+
+// Radix sorts out index by index 
+// We only need to compare 10 spots so from 0 - 9
+void radixS(T* array, int arraySize) {
+  for (int i = 0; i < 10; i++) {
+    // Send an array, size, and place
+    MyFunc(array,i,arraySize);
+  }
+}
+
+struct T {
+  int K[10];
+};
 
 int main(int argc,char **argv) {
 
-  int *Sequence;
   int arraySize;
+  int value;
 
-  // Get the size of the sequence
-  cout << "Enter array size:\n";
-  cin >> arraySize;
+  T* array;
+  array = new T [arraySize];
 
-  // Allocate enough memory to store "arraySize" integers
-  Sequence = new int[arraySize];
-    
-  // Read in the sequence
-  cout << "Enter values:\n";
-  for ( int i=0; i<arraySize; i++ )
-    cin >> Sequence[i];
+  //Making a two dimensional ghetto array to store vector values
+  for (int x = 0; x < arraSize; x++) {
+    // Size of 10 because there's only 0-9 placesible values
+    for (int y = 0; y < 10; y++) {
+      cin >> array[x].K[j];
+    }
+  }
 
-  // Run your algorithms to manipulate the elements in Sequence
+  radixS(array,arraySize);
 
-  MyFunc(Sequence, arraySize); 
-    
-  // Output the result
-  cout << "The sorted sequence is:\n";
-  for(int i=0; i<arraySize; i++)
-      cout << Sequence[i] << endl;
-    
-  // Free allocated space
-  delete[] Sequence;
-}
+  //Output
+  for (int x = 0; x <arraySize; x++) {
+    //Range is from 0 to 10 again cause values range from 0-9
+    for (int y = 0; y < 10; y++) {
+      cout << array[x].key[y] << "\n";
+    }
+  }
+   delete[] Sequence;
+  }
+ 
